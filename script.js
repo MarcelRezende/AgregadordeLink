cdeb40f2d2706427df1dc9fc11438cea7c84ef6a
Marcel <marcelhrez2010@gmail.com>
2026-05-21 15:50:50 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-IA-2.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
+
+//if (html.classList.contains("light")) {
+//  html.classList.remove("light")
+// } else {
+//   html.classList.add("light")
+// }
+
+// TOGGLE SUBSTITUI ISSO TUDO!!!!!!
+
+// PEGAR A TAG IMG
